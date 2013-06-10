770b5e225c2fadf5f4003a1677739300f32899d9
Nathan Demick <ndemick@covermymeds.com>
2013-06-10 17:31:20 -0400

    Now using Grunt as build system

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+/*jslint sloppy: true */
+/*global module: false */
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                src: [
+                    'src/vectr.game.js',
+                    'src/vectr.button.js',
+                    'src/vectr.emitter.js',
+                    'src/vectr.label.js',
+                    'src/vectr.pool.js',
+                    'src/vectr.scene.js',
+                    'src/vectr.shape.js'
+                ],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            dist: {
+                src: 'dist/<%= pkg.name %>.js',
+                dest: 'dist/<%= pkg.name %>.min.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task
+    grunt.registerTask('default', ['concat', 'uglify']);
+};
