d93f0f257dabde25375029785536144b0449a4ed
endemic <ndemick@gmail.com>
2013-10-21 16:56:21 -0400

    Add Jasmine for tests

---
+++ spec/TestSpec.js
@@ -0,0 +1,8 @@
+/*jslint sloppy: true */
+/*global describe: false, it: false, expect: false */
+
+describe('A suite', function () {
+    it('contains spec with an expectation', function () {
+        expect(true).toBe(true);
+    });
+});
