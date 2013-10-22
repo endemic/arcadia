c7fece539f1db887171f7c9fcf145353c6d36ab1
endemic <ndemick@gmail.com>
2013-10-22 06:34:36 -0400

    Minor cleanup

--- example/player-bullet.js
+++ example/player-bullet.js
@@ -1,17 +1,11 @@
 /*jslint sloppy: true, browser: true */
-/*globals Vectr, Player */
+/*globals Vectr: false */
 
 var PlayerBullet = function () {
     Vectr.Shape.apply(this, arguments);
 
     this.shape = 'square';
     this.size = 2;
-    this.colors = {
-        'red': 255,
-        'blue': 255,
-        'green': 255,
-        'alpha': 1
-    };
     this.lineWidth = 3;
     this.speed = 150;
     this.velocity.y = -1;
