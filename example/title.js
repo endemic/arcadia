cfdcf7f0e570e623195033258d1e679ed2fd6dab
endemic <ndemick@gmail.com>
2014-04-15 07:03:22 -0400

    Changes in example game

--- example/title.js
+++ example/title.js
@@ -3,30 +3,36 @@
 
 var Title = function () {
     Arcadia.Scene.apply(this, arguments);
-    this.clearColor = 'rgba(0, 0, 0, 0.15)';
+    this.clearColor = 'rgba(0, 0, 0, 0.10)';
 
     var title = new Arcadia.Label(Arcadia.WIDTH / 2, Arcadia.HEIGHT / 4, "ARMADA");
     title.font = '40px monospace';
     title.color = 'rgba(255, 255, 255, 0.8)';
-    title.glow = 10;
-    this.add(title);
+    title.shadow.x = 0;
+    title.shadow.y = 0;
+    title.shadow.blur = 10;
+    title.shadow.color = 'rgba(255, 255, 255, 0.5)';
+    this.children.add(title);
 
     this.button = new Arcadia.Button(Arcadia.WIDTH / 2, Arcadia.HEIGHT / 2, "START"); // x, y, text
     this.button.font = '20px monospace';
     this.button.solid = false;
     this.button.padding = 20;
-    this.button.glow = 20;
+    this.button.shadow.x = 0;
+    this.button.shadow.y = 0;
+    this.button.shadow.blur = 10;
+    this.button.shadow.color = 'rgba(255, 255, 255, 0.5)';
     this.button.onUp = function () {
         Arcadia.changeScene(Game);
     };
-    this.add(this.button);
+    this.children.add(this.button);
 
     // Add a starfield background
     this.stars = new Arcadia.Pool();
     this.stars.factory = function () {
         var star = new Arcadia.Shape(Math.random() * Arcadia.WIDTH, Math.random() * Arcadia.HEIGHT, 'circle', Math.random() * 4 + 1);
         star.solid = true;
-        star.velocity.y = 40 / star.size;
+        star.velocity.y = 60 / star.size;
         star.update = function (delta) {
             Arcadia.Shape.prototype.update.call(this, delta);   // "super"
 
@@ -39,7 +45,7 @@ var Title = function () {
         return star;
     };
 
-    this.add(this.stars);
+    this.children.add(this.stars);
 
     // Create 50 star objects
     while (this.stars.length < 50) {
